710e6d8aa2a87ec1e2034c403d00d7b3d6f43a5f
ranesimons <rane@thirdpartytrust.com>
2021-09-10 01:27:55 -0500

    connected the backend

--- Backend/server.js
+++ Backend/server.js
@@ -13,21 +13,61 @@ const io = socketio(server);
 // Run when a client connects
 io.on('connection', socket => {
     socket.emit('message', 'Welcome To Transcribr');
+    console.log('Welcome To Transcribr');
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'User has joined the chat');
+    // socket.broadcast.emit('message', 'User has joined the chat');
 
     // Runs when a client disconnects
 
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
-    });
+    // socket.on('disconnect', () => {
+    //     io.emit('message', 'A user has left the chat');
+    // });
 
     // Runs when a user sends a message
-    socket.on('chatMessage', (msg) => {
+    socket.on('new message', (msg) => {
         io.emit('message', msg);
+        console.log(msg);
     });
 });
 
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// Setup
+// var express = require('express')
+// var app = express()
+// var path = require('path');
+// var server = require('http').createServer(app);
+// var io = require('../..')(server);
+// var port = process.env.PORT || 3000;
+
+// server.listen(port, () => {
+//     console.log('Server listening at port %d', port);
+// });
+
+// // Route
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Chats
+
+// var numUsers = 0;
+
+// io.on('connection', (socket) => {
+//     // var addedUser = false;
+
+//     // new message handler
+//     socket.on('new message', (data) => {
+//         // execute
+//         socket.broadcast.emit('new message', {
+//             message: data
+//         });
+//     })
+
+//     // disconnect from socket
+//     // socket.on('disconnect', () => {
+//     //     if
+//     // })
+// })
--- Transcribr/ViewController.swift
+++ Transcribr/ViewController.swift
@@ -5,38 +5,108 @@
 //  Created by Rane Simons on 8/27/21.
 //
 
+import Foundation
 import UIKit
 import AVFoundation
 import Speech
+import SocketIO
+
+struct SocketMessage: Codable {
+    var message: String
+    var username: String
+}
+
+class SocketParser {
+
+    static func convert<T: Decodable>(data: Any) throws -> T {
+        let jsonData = try JSONSerialization.data(withJSONObject: data)
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: jsonData)
+    }
+    
+    static func convert<T: Decodable>(datas: [Any]) throws -> [T] {
+        return try datas.map { (dict) -> T in
+            let jsonData = try JSONSerialization.data(withJSONObject: dict)
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: jsonData)
+        }
+    }
+}
+
+class SocketChatManager {
+    
+    // properties
+    let manager = SocketManager(socketURL: URL(string: "ws://localhost:3000")!, config: [.log(true), .compress])
+    var socket: SocketIOClient? = nil
+    
+    // lives
+    
+    init() {
+        setupSocket()
+        setupSocketEvents()
+        socket?.connect()
+    }
+    
+    func stop() {
+        socket?.removeAllHandlers()
+    }
+    
+    // setup
+    
+    func setupSocket() {
+        self.socket = manager.defaultSocket
+    }
+    
+    func setupSocketEvents() {
+        socket?.on(clientEvent: .connect) {data, ack in
+            print("Connected")
+        }
+
+//        socket?.on("login") { (data, ack) in
+//            guard let dataInfo = data.first else { return }
+//            if let response: SocketLogin = try? SocketParser.convert(data: dataInfo) {
+//                print("Now this chat has \(response.numUsers) users.")
+//            }
+//
+//        }
+//
+//        socket?.on("user joined") { (data, ack) in
+//            guard let dataInfo = data.first else { return }
+//            if let response: SocketUserJoin = try? SocketParser.convert(data: dataInfo) {
+//                print("User '\(response.username)' joined...")
+//                print("Now this chat has \(response.numUsers) users.")
+//            }
+//        }
+        
+        socket?.on("new message") { (data, ack) in
+            guard let dataInfo = data.first else { return }
+            if let response: SocketMessage = try? SocketParser.convert(data: dataInfo) {
+//                print("Message from '\(response.username)': \(response.message)")
+                print("\(response.message)")
+            }
+            
+        }
+    }
+
+    func send(message: String) {
+        socket?.emit("new message", message)
+    }
+
+}
 
 class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDelegate, UITableViewDataSource {
 
+    var numberOfMessages:Int = 0
+    var words:[String] = []
     var recordingSession:AVAudioSession!
     var audioRecorder:AVAudioRecorder!
     var audioPlayer:AVAudioPlayer!
-//    var player: AVAudioPlayer?
+    var chats:SocketChatManager!
     var numberOfRecordings:Int = 0
     @IBOutlet weak var recordAudioLabel: UIButton!
     @IBOutlet weak var seeAudioRecordings: UITableView!
-    
-    func deleteAudioFiles()
-    {
-        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        print(documentsUrl)
-        do {
-            let fileURLs = try FileManager.default.contentsOfDirectory(at: documentsUrl,
-                                                                       includingPropertiesForKeys: nil,
-                                                                       options: .skipsHiddenFiles)
-            print(fileURLs)
-            print("wat")
-            for fileURL in fileURLs {
-                if fileURL.pathExtension == "m4a" {
-                    try FileManager.default.removeItem(at: fileURL)
-                }
-            }
-        } catch  { print(error) }
-    }
-    
+    @IBOutlet var prototypeChatMessages: UITableView!
+
     @IBAction func recordTheAudio(_ sender: Any) {
         if audioRecorder == nil
         {
@@ -71,6 +141,24 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
         }
     }
     
+    func deleteAudioFiles()
+    {
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//        print(documentsUrl)
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: documentsUrl,
+                                                                       includingPropertiesForKeys: nil,
+                                                                       options: .skipsHiddenFiles)
+            print(fileURLs)
+            print("wat")
+            for fileURL in fileURLs {
+                if fileURL.pathExtension == "m4a" {
+                    try FileManager.default.removeItem(at: fileURL)
+                }
+            }
+        } catch  { print(error) }
+    }
+    
     func requestTranscribePermissions() {
         SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
             DispatchQueue.main.async {
@@ -87,6 +175,9 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
         super.viewDidLoad()
         // Do any additional setup after loading the view
         recordingSession = AVAudioSession.sharedInstance()
+        prototypeChatMessages.register(UITableViewCell.self, forCellReuseIdentifier: "proto")
+        prototypeChatMessages.delegate = self
+        prototypeChatMessages.dataSource = self
         
         if let count:Int = UserDefaults.standard.object(forKey: "num") as? Int
         {
@@ -98,6 +189,8 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
                 print ("ACCEPTED")
             }
         }
+        
+        chats = SocketChatManager()
         
         requestTranscribePermissions()
     }
@@ -121,14 +214,34 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
     // Table View Setup
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRecordings
+        return numberOfMessages
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let proto = tableView.dequeueReusableCell(withIdentifier: "proto", for: indexPath)
-        proto.textLabel?.text = String(indexPath.row + 1)
+        proto.textLabel?.text = String(words[indexPath.row])
         return proto
     }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeReusableCellWithIdentifier("idCellChat", forIndexPath: indexPath) as! ChatCell
+//        let currentChatMessage = chatMessages[indexPath.row]
+//        let senderNickname = currentChatMessage["nickname"] as! String
+//        let message = currentChatMessage["message"] as! String
+//        let messageDate = currentChatMessage["date"] as! String
+        
+//        if senderNickname == nickname {
+//            cell.lblChatMessage.textAlignment = NSTextAlignment.Right
+//            cell.lblMessageDetails.textAlignment = NSTextAlignment.Right
+//            cell.lblChatMessage.textColor = lblNewsBanner.backgroundColor
+//        }
+        
+//        cell.lblChatMessage.text = message
+//        cell.lblMessageDetails.text = "by \(senderNickname.uppercaseString) @ \(messageDate)"
+//        cell.lblChatMessage.textColor = UIColor.darkGrayColor()
+        
+//        return cell
+//    }
     
     func transcribeAudio(url: URL) {
         // create a new recognizer and point it at our audio
@@ -148,27 +261,34 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
             // if we got the final transcription back, print it
             if result.isFinal {
                 // pull out the best transcription...
-                print(result.bestTranscription.formattedString)
+                let transcribing = result.bestTranscription.formattedString
+                print(transcribing)
+                chats.send(message: transcribing)
+                numberOfMessages += 1
+                words.append(transcribing)
+                print(words)
+                prototypeChatMessages.reloadData()
+//                print(result.bestTranscription.formattedString)
                 deleteAudioFiles()
             }
         }
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      let sound = getFileDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
-//        let sound = getFileDirectory().appendingPathComponent("\(indexPath.row + 1).mp4a")
-        
-        do
-        {
-            audioPlayer = try AVAudioPlayer(contentsOf: sound)
-            audioPlayer.play()
-            transcribeAudio(url: sound)
-        }
-        catch
-        {
-            print(error.localizedDescription)
-        }
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//      let sound = getFileDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
+//      let sound = getFileDirectory().appendingPathComponent("\(indexPath.row + 1).mp4a")
+//        
+//        do
+//        {
+//            audioPlayer = try AVAudioPlayer(contentsOf: sound)
+//            audioPlayer.play()
+//            transcribeAudio(url: sound)
+//        }
+//        catch
+//        {
+//            print(error.localizedDescription)
+//        }
+//    }
     
 }
 
